651083e9eabe29543020bac2f0f142d18204eee2
osias10 <wnsguddk1@naver.com>
2021-09-23 01:20:25 +0900

    youtube 링크를 받아 재생

--- src/utils/musicutils.js
+++ src/utils/musicutils.js
@@ -20,7 +20,11 @@ async function music(msg){
     console.log("커맨드1");
     console.log(commandList[1]);
     if (commandList[0].startsWith('p')){
-        play(commandList[1],connection);
+        try{
+            play(commandList[1],connection);
+        } catch (e) {
+            console.log(e);
+        }
     }else if (msg.content.startsWith(`${prefix}skip`)) {
         
         return;
@@ -46,11 +50,15 @@ async function play(arg,connection){
     const stream = ytdl(arg, {
         filter: "audioonly"
     });
+    try{
+        const player = createAudioPlayer();
+        const resource = createAudioResource(stream);
+        await player.play(resource);
+        connection.subscribe(player);
+    }catch (e){
+        console.log(e);
+    }
     
-    const player = createAudioPlayer();
-    const resource = createAudioResource(stream);
-    await player.play(resource);
-    connection.subscribe(player);
 }
 
 module.exports= {
